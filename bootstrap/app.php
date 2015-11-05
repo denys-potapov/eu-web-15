a7089fd9a68d8bd19152fcb3300bfa3e495fab26
Denys Potapov <denys.potapov@gmail.com>
2015-11-05 16:39:35 +0200

    queue migrations'

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-// Dotenv::load(__DIR__.'/../');
+Dotenv::load(__DIR__.'/../');
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +16,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 */
 
 $app = new Laravel\Lumen\Application(
-    realpath(__DIR__.'/../')
+  realpath(__DIR__.'/../')
 );
 
 // $app->withFacades();
